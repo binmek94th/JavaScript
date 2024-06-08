ec95c4dc6e28de556f1de38dfa0396875af17d3c
Nehemiah Mekonnen <binyamm83@gmail.com>
2024-06-08 12:21:57 +0300

    Updated

---
+++ index.js
@@ -0,0 +1,13 @@
+let age = 30.1;
+let isApproved = true;
+let firstName = undefined;
+let lastName = null;
+console.log(typeof age);
+age = 'Thirty';
+console.log(typeof age);
+let person = {
+    name : 'Bin',
+    age : '30'
+};
+let selectedColors = [];
+
